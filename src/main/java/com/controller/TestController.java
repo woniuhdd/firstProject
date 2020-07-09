df31e17323adb9183da7facbedd0ba499af180bf
zhankun <646940594@qq.com>
2020-07-09 20:48:02 +0800

    11

--- src/main/java/com/controller/TestController.java
+++ src/main/java/com/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.sys.model.SysUser;
+import com.sys.service.SysUserManager;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     protected static final String ACTION_PATH = "/test";
     private static final Logger log = Logger.getLogger(TestController.class);
+
+    @Autowired
+    private SysUserManager sysUserManager;
 
     /**
      * 连通性测试：测试是否能调用到接口
@@ -26,6 +32,11 @@ public class TestController {
         return returnJsonObj;
     }
 
-
+    @RequestMapping(value = "/test", method = {RequestMethod.POST})
+    @ResponseBody
+    public SysUser test() {
+        SysUser sysUser = sysUserManager.getUserByUsername("ZBZX");
+        return sysUser;
+    }
 
 }
--- src/main/java/com/sys/dao/SysUserDao.java
+++ src/main/java/com/sys/dao/SysUserDao.java
@@ -3,9 +3,10 @@ package com.sys.dao;
 import com.common.dao.GenericDao;
 import com.sys.model.SysUser;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface SysUserDao  extends GenericDao<SysUser,String>{
 
-    SysUser getUserByUsername(String username);
+    SysUser getUserByUsername(@Param("userId") String username);
 }
