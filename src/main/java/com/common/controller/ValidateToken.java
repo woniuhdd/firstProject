4789792626c4c81b37cd8cd690903a9005da3dff
hdd <1947661545@qq.com>
2020-11-30 09:50:40 +0800

    接口权限配置修改

--- src/main/java/com/common/controller/ValidateToken.java
+++ src/main/java/com/common/controller/ValidateToken.java
@@ -1,15 +1,16 @@
 package com.common.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.common.utils.Base64Util;
 import com.common.utils.JwtTokenUtil;
 import com.enums.ResultCode;
 import com.sys.model.SysInterfacePrivilege;
 import com.sys.model.SysUser;
-import com.sys.service.SysInterfacePrivilegeManager;
 import com.sys.service.SysUserManager;
 import io.jsonwebtoken.Claims;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -25,12 +26,13 @@ public class ValidateToken {
     @Autowired
     private SysUserManager sysUserManager;
     @Autowired
-    private SysInterfacePrivilegeManager sysInterfacePrivilegeManager;
-    @Autowired
     private HttpServletRequest request;
+    @Value("${interface.Privilege}")
+    private String interfacePrivilege;
 
     public Map<String, Object> validateToken(String token) {
         Map<String, Object> map = new HashMap<>();
+        List<SysInterfacePrivilege>  sysInterfacePrivilegeList= JSONObject.parseArray(interfacePrivilege, SysInterfacePrivilege.class);
 
         //验证token数据格式
         if (StringUtils.isEmpty(token)){
@@ -71,23 +73,36 @@ public class ValidateToken {
         SysUser sysUser = sysUserManager.getUserByUsername(username);
 
         //验证是否有接口访问权限
+//        String urlPath = request.getServletPath();
+//        Map<String ,Object> params = new HashMap<>();
+//        params.put("orgId",sysUser.getOrgid());
+//        params.put("urlPath",urlPath);
+//        List<SysInterfacePrivilege> sysInterfacePrivilegeList = sysInterfacePrivilegeManager.getListByParams(params);
+//        if (sysInterfacePrivilegeList.size() != 1){
+//            map.put("resultCode", ResultCode.PERMISSION_UNAUTHORISE.getCode());
+//            map.put("resultMsg", ResultCode.PERMISSION_UNAUTHORISE.getMessage());
+//            return map;
+//        }else {
+//            if ("0".equals(sysInterfacePrivilegeList.get(0).getIsUsing())){
+//                map.put("resultCode", ResultCode.PERMISSION_UNAUTHORISE.getCode());
+//                map.put("resultMsg", ResultCode.PERMISSION_UNAUTHORISE.getMessage());
+//                return map;
+//            }
+//        }
+
+        //验证是否有接口访问权限
         String urlPath = request.getServletPath();
-        Map<String ,Object> params = new HashMap<>();
-        params.put("orgId",sysUser.getOrgid());
-        params.put("urlPath",urlPath);
-        List<SysInterfacePrivilege> sysInterfacePrivilegeList = sysInterfacePrivilegeManager.getListByParams(params);
-        if (sysInterfacePrivilegeList.size() != 1){
+        boolean privilege = false;
+        for (SysInterfacePrivilege sysInterfacePrivilege : sysInterfacePrivilegeList){
+            if (sysUser.getOrgid().equals(sysInterfacePrivilege.getOrgId()) && urlPath.equals(sysInterfacePrivilege.getUrlPath())){
+                privilege = true;
+            }
+        }
+        if (!privilege){
             map.put("resultCode", ResultCode.PERMISSION_UNAUTHORISE.getCode());
             map.put("resultMsg", ResultCode.PERMISSION_UNAUTHORISE.getMessage());
             return map;
-        }else {
-            if ("0".equals(sysInterfacePrivilegeList.get(0).getIsUsing())){
-                map.put("resultCode", ResultCode.PERMISSION_UNAUTHORISE.getCode());
-                map.put("resultMsg", ResultCode.PERMISSION_UNAUTHORISE.getMessage());
-                return map;
-            }
         }
-
 
         map.put("orgId",sysUser.getOrgid());
         map.put("userId", sysUser.getUserid());
--- src/main/java/com/sys/dao/SysInterfacePrivilegeDao.java
+++
@@ -1,9 +0,0 @@
-package com.sys.dao;
-
-import com.common.dao.GenericDao;
-import com.sys.model.SysInterfacePrivilege;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SysInterfacePrivilegeDao extends GenericDao<SysInterfacePrivilege, String> {
-}
